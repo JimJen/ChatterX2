3566b38f5f66a243753e7dd891325b2f0aedf7b5
JKSayers <jsayers1980@gmail.com>
2017-03-01 11:33:08 -0500

    Added models etc

---
+++ ChatterX2/Models/Chat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ChatterX2.Models
+{
+    public class Chat
+    {
+        public int ID { get; set; }
+        [MaxLength(150)]
+
+        public string Chats { get; set; }
+        public virtual ApplicationUser Username { get; set; }
+    }
+}
--- ChatterX2/Models/IdentityModels.cs
+++ ChatterX2/Models/IdentityModels.cs
@@ -3,12 +3,25 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
 
 namespace ChatterX2.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+
+        public override string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Bio { get; set; }
+        public virtual ICollection<Chat> Chats { get; set; }
+
+
+
+
+
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -29,5 +42,7 @@ namespace ChatterX2.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<ChatterX2.Models.Chat> Chats { get; set; }
     }
 }
