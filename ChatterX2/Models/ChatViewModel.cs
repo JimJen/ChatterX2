c1e78309d07b98c8bdcd59879ddd9fb4af90796b
Jen Krejsa <jlkrejsa@gmail.com>
2017-03-01 15:15:54 -0500

    updated commit

---
+++ ChatterX2/Models/ChatViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatterX2.Models
+{
+    public class ChatViewModel
+    {
+        public Chat ChatView { get; set; }
+        public ApplicationUser AppUser { get; set; }
+    }
+}
